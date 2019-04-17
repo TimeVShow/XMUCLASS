334261240066ef7685e797b3a29a25ac16971b25
LZY <1247723271@qq.com>
2019-04-17 21:09:01 +0800

    add flex&&read database&&get data

--- miniprogram/pages/classroom/classroom.js
+++ miniprogram/pages/classroom/classroom.js
@@ -9,7 +9,12 @@ Page({
     area:[{"id":0,"text":"海韵教学楼","position":"haiyunjiaoxuelou"},{"id":1,"text":"学生公寓","position":"xueshenggongyu"}],
     time: ["第1-2节", "第3-4节", "第5-6节", "第7-8节", "第9-11节"],
     whichweek:0,
-    start_time:"2019/2/16"
+    start_time:"2019/2/16",
+    ot:[],
+    tf:[],
+    fs:[],
+    se:[],
+    ne:[]
     },
   /**
    * 生命周期函数--监听页面加载
@@ -68,11 +73,13 @@ Page({
   //得到下拉列表的返回值，并开始进行处理
   getDate: function (e) {
     var a=e.detail["id"];
+    console.log(a);
     var area=this.data.area[a]["position"];
+    console.log(area);
     var day=this.today();
-    var date=new Date();
-    this.getweekString(date);
-    this.work(area,day);
+    var date=new Date();    
+    var week=this.getweekString(date);
+    this.work(week,area,day);
   },
   //得到当前是星期几
   today:function()
@@ -81,15 +88,48 @@ Page({
     a = a[0] + a[1] + a[2];
     return a;
   },
-  work:function(area,day)
+  work:function(week,area,day)
   {
-    const cl=["1-2","3-4","5-6","7-8","9-11"];
+    const name=["ot","tf","fs","se","ne"];
+    const cl=["1_2","3_4","5_6","7_8","9_11"];
     const db = wx.cloud.database({ env: 'classroom-messege-78b0bb' });
-    const position = db.collection('haiyunjiaoxuelou');
+    console.log(area);
+    const position = db.collection(area);
+    console.log(position);
+    var b=[];
+    var that=this;
     db.collection(area).where({_id:day}).get({
       success(res)
       {
-        console.log(res.data[0]["1-2"][0]["flag"]);
+        console.log(res.data);
+        var len=res.data[0]["length"];
+        var i=0;
+        var k=0;
+        for(k;k<cl.length;k=k+1)
+        {
+          var count=0;
+          for(i=0;i<len;i=i+1)
+          {
+            var fl=false;
+            if(res.data[0][cl[k]][i]["flag"])
+            {
+              for (let j = 0; j < res.data[0][cl[k]][i]["special"].length;j++)
+              {
+                if (week == res.data[0][cl[k]][i]["special"][j])
+                {
+                  fl=true;
+                  break;
+                }
+              }
+              if(!fl)
+              {
+                b[count]=res.data[0][cl[k]][i]["room"];
+                count = count + 1;
+              }
+            }
+          }
+          that.setData({[name[k]]:b});
+        }
       }
     });
   },
